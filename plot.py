75be19f38fc3de506bcc2e76d92c36cc10911721
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-11-22 14:43:42 -0300

    correção na função

--- plot.py
+++ plot.py
@@ -1,13 +1,22 @@
+
 import matplotlib.pyplot as plt
 
-file = open("../Testes/1/tst.txt", "r")
+plt.style.use('Solarize_Light2')
+
+file = open("../Testes/1/logs/fitness-log.txt", "r")
+
+fitnessMin = 31
 
 fitness = []
 
 for x in file:
-    fitness.append(int(x))
+    if(int(x) - fitnessMin < 0):
+        continue
+    fitness.append(int(x) - fitnessMin)
 
 count = list(range(len(fitness)))
 
 plt.plot(count, fitness)
 plt.show()
+
+
